9781fc706b211c22195f04d8de243be84dcdfce4
leonam <leonam447@gmail.com>
2026-04-06 15:29:05 -0300

    Add ConsentApplication

---
+++ src/main/java/top/lmix/consentimento/ConsentimentoApplication.java
@@ -0,0 +1,13 @@
+package top.lmix.consentimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsentimentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsentimentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/lmix/consentimento/ConsentimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lmix.consentimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsentimentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
